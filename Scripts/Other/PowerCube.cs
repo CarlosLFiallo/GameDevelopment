849e7bf5ed9b56857c1b2dc840f4fe4e31a3bd40
Dingus BingleThorn <141524970+CarlosLFiallo@users.noreply.github.com>
2023-08-09 17:30:42 -0400

    Add files via upload
    Newest Code as of 8/9/2023

---
+++ Scripts/Other/PowerCube.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerCube : MonoBehaviour
+{
+    public float fireRatePower = 1;
+    public float dashRatePower = 1;
+    public float speedPower = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
